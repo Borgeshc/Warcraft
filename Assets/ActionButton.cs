0090094b7e35c6ca9f213c6163e2855b9b8df2d2
Borgeshc <Borgeshc@gmatc.matc.edu>
2018-02-19 02:26:51 -0600

    Added action button ui

---
+++ Assets/ActionButton.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ActionButton : MonoBehaviour
+{
+    public Image icon;
+    public KeyCode keybind;
+
+    InputManager inputManager;
+
+    private void Start()
+    {
+        inputManager = GameObject.Find("Player").GetComponent<InputManager>();
+    }
+
+    void Update ()
+    {
+	    if(Input.GetKeyDown(keybind))
+        {
+            inputManager.ActionButtonPressed();
+        }
+	}
+
+    public void ActionButtonPressed()
+    {
+        inputManager.ActionButtonPressed();
+    }
+
+    public void SetIconImage(Sprite image)
+    {
+        icon.sprite = image;
+    }
+}
