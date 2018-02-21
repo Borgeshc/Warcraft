e79d57291871efbe4ce9019d6d6c63b727e428d0
borgeshc <borgeshc@gmatc.matc.edu>
2018-02-21 11:29:51 -0600

    Gameplay updates
    *Added explosive trap

---
+++ Assets/DealDamageOnTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DealDamageOnTrigger : Trap
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag.Equals("Enemy"))
+        {
+            int randomExplosiveDamage = Random.Range(minDamage, maxDamage);
+            other.transform.GetComponent<Health>().TookDamage(randomExplosiveDamage, stats.criticalStrikeChance, stats.criticalStrikeDamage, namePlate);
+            Destroy(gameObject);
+        }
+    }
+}
